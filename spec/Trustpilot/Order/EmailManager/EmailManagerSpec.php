2b58df0fd5e6580a2381267c0c5ba38a5e51bec8
Joachim Løvgaard <joachim@loevgaard.dk>
2018-11-07 13:27:52 +0100

    added return type and fixed style

--- spec/Trustpilot/Order/EmailManager/EmailManagerSpec.php
+++ spec/Trustpilot/Order/EmailManager/EmailManagerSpec.php
@@ -4,14 +4,13 @@ namespace spec\Setono\SyliusTrustpilotPlugin\Trustpilot\Order\EmailManager;
 
 use Setono\SyliusTrustpilotPlugin\Trustpilot\Order\EmailManager\EmailManager;
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 use Sylius\Component\Core\Model\CustomerInterface;
 use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Mailer\Sender\SenderInterface;
 
 class EmailManagerSpec extends ObjectBehavior
 {
-    public function it_is_initializable()
+    public function it_is_initializable(): void
     {
         $this->shouldHaveType(EmailManager::class);
     }
