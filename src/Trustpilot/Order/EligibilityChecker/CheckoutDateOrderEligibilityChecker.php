264b34ef3ba1aae0f45ad86d2f75f1723b027e7a
Igor Mukhin <igor.mukhin@gmail.com>
2018-11-09 00:41:48 +0200

    Added: One more forgotten checker to check order checkout date

---
+++ src/Trustpilot/Order/EligibilityChecker/CheckoutDateOrderEligibilityChecker.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusTrustpilotPlugin\Trustpilot\Order\EligibilityChecker;
+
+use Setono\SyliusTrustpilotPlugin\Model\OrderTrustpilotAwareInterface;
+
+final class CheckoutDateOrderEligibilityChecker implements OrderEligibilityCheckerInterface
+{
+    /**
+     * @var int
+     */
+    private $sendInDays;
+
+    /**
+     * CheckoutDateOrderEligibilityChecker constructor.
+     * @param int $sendInDays
+     */
+    public function __construct(int $sendInDays)
+    {
+        $this->sendInDays = $sendInDays;
+    }
+
+    /**
+     * Eligible only when order checkout completed given amount of days ago
+     *
+     * {@inheritdoc}
+     */
+    public function isEligible(OrderTrustpilotAwareInterface $order): bool
+    {
+        if (null === $order->getCheckoutCompletedAt()) {
+            return false;
+        }
+
+        $pastDate = new \DateTime(sprintf('-%s day', $this->sendInDays));
+        return $order->getCheckoutCompletedAt()->getTimestamp() <= $pastDate->getTimestamp();
+    }
+}
